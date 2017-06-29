369f73e9f0b148a448a306be3cbee1cbcf9e7e9a
Oscar Tu <o.car.w2@gmail.com>
2017-06-29 01:55:55 -0700

    Clean up UI

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -72,10 +72,4 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    
-    
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
 }
