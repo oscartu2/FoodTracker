dbe8bede34b178cc5bf6e9512ea4e0e3dfa70ec9
Oscar Tu <o.car.w2@gmail.com>
2017-07-18 02:43:58 -0700

    Finish implementing save/cancel navigation.

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -30,6 +30,10 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
+        
+        // Enable the Save button only if the text field has a valid Meal name
+        
+        updateSaveButtonState()
     }
     
     //MARK: UITextFieldDelegate
@@ -41,6 +45,17 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        updateSaveButtonState()
+        navigationItem.title = textField.text
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        // Disable the save button while editing.
+        
+        saveButton.isEnabled = false
         
     }
     
@@ -66,6 +81,12 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     }
     
     //MARK: Navigation
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
     
     // This method lets you configure a view controlled before it's presented.
     
@@ -104,5 +125,16 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Make sure ViewController is notified when the user picks an image.
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    //MARK: Private Methods
+    
+    private func updateSaveButtonState() {
+        
+        // Disable the Save button if the text field is empty.
+        
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
+        
     }
 }
